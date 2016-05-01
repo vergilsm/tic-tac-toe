e0577b463ccbfe0f51c4d2037eca8efce2cb6417
Dmitriy Smirnov <Dmitriy Smirnov>
2016-05-02 00:09:22 +0300

    version-3 change class Game, delete file

--- game.rb
+++ game.rb
@@ -1,5 +1,3 @@
-# gem unicode_utils для преобразования в нижний регистр слова и букв вводимых юзером
-require "unicode_utils/downcase"
 class Game
   def initialize
     # Объявляю пустой массив для записи в него всех строк файла
@@ -8,10 +6,12 @@ class Game
     @player_moves = []
     # Массив для записи ходов компьютера
     @comp_moves = []
-    # Массив для записи ходов игрока-1
-    @name1_moves = []
-    # Массив для записи ходов игрока-2
-    @name2_moves = []
+    # хеш всех ходов для проверки пользователя.
+    @all_moves = {1 => "x1 y1", 2 => "x2 y1", 3 =>"x3 y1", 4 => "x1 y2", 5 => "x2 y2",
+                 6 => "x3 y2", 7 => "x1 y3", 8 => "x2 y3", 9 => "x3 y3"}
+    # хеш с выигрышными комбинациями
+    @positions_wins = {1=> [1,2,3], 2=> [4,5,6], 3=> [7,8,9], 4=> [1,4,7],
+                      5=> [2,5,8], 6=> [3,6,9], 7=> [1,5,9], 8=> [3,5,7]}
   end
 
   # Метод хранящий игровое поле и поле, для красоты выводимое на экран
@@ -31,7 +31,7 @@ class Game
       # Возвращаю наглядное поле
       # Создаю клон поля и удаляю из него все #
       clone_all_lines = @all_lines_field.clone
-      line2 = clone_all_lines[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
+      line2 = clone_all_lines[2].gsub(/[123]/, ' ')
       clone_all_lines[2] = line2
 
       line6 = clone_all_lines[6].gsub(/[456]/, ' ')
@@ -46,334 +46,78 @@ class Game
     end
   end # Финал метода field
 
-  # Вариант игры - юзер против компьютера
-  def player_vs_comp
-    UnicodeUtils.downcase("D")
+  def move_player(move_player)
 
-    # хеш всех ходов для проверки пользователя.
-    all_moves = {1 => "x1 y1", 2 => "x2 y1", 3 =>"x3 y1", 4 => "x1 y2", 5 => "x2 y2",
-                 6 => "x3 y2", 7 => "x1 y3", 8 => "x2 y3", 9 => "x3 y3"}
+    @player_moves << move_player
 
-    # хеш с выигрышными комбинациями
-    positions_wins = {1=> [1,2,3], 2=> [4,5,6], 3=> [7,8,9], 4=> [1,4,7],
-                      5=> [2,5,8], 6=> [3,6,9], 7=> [1,5,9], 8=> [3,5,7]}
-    # Если пользователь выбрал крестики.
-    # Пока юзер не сделал 5 ходов
-    while @player_moves.size <= 4
-      # Прошу пользователя сделать ход
-      puts "\nКак делать ход! Введите (лат.) x1 y1 и Enter\n\nСделайте ваш ход.\n\n"
-      move_player = STDIN.gets.chomp.downcase
-      # Проверка, чтобы юзер не ввел ерунду
-      while !all_moves.value?(move_player) do
-        puts "\nСделайте ваш ход. (Как делать ход: x1 y1 и Enter)\n"
-        move_player = STDIN.gets.chomp
-      end
-      # Записываю ход в массив и вывожу игровое поле на экран
-      case move_player
-        when 'x1 y1'
-          @player_moves << 1
-          move = @all_lines_field[2].sub('1', 'X')
-          @all_lines_field[2] = move
+    l2 = @all_lines_field[2].sub(move_player.to_s, "X")
+    l6 = @all_lines_field[6].sub(move_player.to_s, "X")
+    l10 = @all_lines_field[10].sub(move_player.to_s, "X")
+    @all_lines_field[2] = l2
+    @all_lines_field[6] = l6
+    @all_lines_field[10] = l10
 
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub('2', ' ').sub('3', ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
+    field_new = Array.new(@all_lines_field)
+    line2 = field_new[2].sub(move_player.to_s, 'X').gsub(/[123]/, ' ')
+    line6 = field_new[6].sub(move_player.to_s, 'X').gsub(/[456]/, ' ')
+    line10 = field_new[10].sub(move_player.to_s, 'X').gsub(/[789]/, ' ')
+    field_new[2] = line2
+    field_new[6] = line6
+    field_new[10] = line10
 
-          puts all_lines_field_new
-        when 'x2 y1'
-          @player_moves << 2
-          move = @all_lines_field[2].sub('2', 'X')
-          @all_lines_field[2] = move
+    puts field_new
+  end
 
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').sub('2', 'X').sub('3', ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
+  def move_comp(move_comp)
 
-          puts all_lines_field_new
-        when 'x3 y1'
-          @player_moves << 3
-          move = @all_lines_field[2].sub('3', 'X')
-          @all_lines_field[2] = move
+    @comp_moves << move_comp
 
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').sub('2', ' ').sub('3', 'X')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
+    l2 = @all_lines_field[2].sub(move_comp.to_s, "O")
+    l6 = @all_lines_field[6].sub(move_comp.to_s, "O")
+    l10 = @all_lines_field[10].sub(move_comp.to_s, "O")
+    @all_lines_field[2] = l2
+    @all_lines_field[6] = l6
+    @all_lines_field[10] = l10
 
-          puts all_lines_field_new
-        when 'x1 y2'
-          @player_moves << 4
-          move = @all_lines_field[6].sub('4', 'X')
-          @all_lines_field[6] = move
+    field_new = Array.new(@all_lines_field)
+    line2 = field_new[2].sub(move_comp.to_s, 'O').gsub(/[123]/, ' ')
+    line6 = field_new[6].sub(move_comp.to_s, 'O').gsub(/[456]/, ' ')
+    line10 = field_new[10].sub(move_comp.to_s, 'O').gsub(/[789]/, ' ')
+    field_new[2] = line2
+    field_new[6] = line6
+    field_new[10] = line10
 
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', 'X').sub('5', ' ').sub('6', ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
+    puts field_new
+  end
+  # Метод показывает варианты выигрыша
+  def win(comp_or_player)
+    # Прохожусь по выигрышным массивам
+    @positions_wins.each_value do |win|
+      # Если вместо атрибута метода будет выбран, массив ходов юзера
+      # и если одно из значений(массив выигрышных комбинаций) минус
+      # @player_moves будет равно []
+      if comp_or_player == @player_moves &&
+          win - comp_or_player == []
+        puts "Вы выиграли!"
+        abort
 
-          puts all_lines_field_new
-        when 'x2 y2'
-          @player_moves << 5
-          move = @all_lines_field[6].sub('5', 'X')
-          @all_lines_field[6] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', ' ').sub('5', 'X').sub('6', ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when 'x3 y2'
-          @player_moves << 6
-          move = @all_lines_field[6].sub('6', 'X')
-          @all_lines_field[6] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', ' ').sub('5', ' ').sub('6', 'X')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when 'x1 y3'
-          @player_moves << 7
-          move = @all_lines_field[10].sub('7', 'X')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', 'X').sub('8', ' ').sub('9', ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when 'x2 y3'
-          @player_moves << 8
-          move = @all_lines_field[10].sub('8', 'X')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', ' ').sub('8', 'X').sub('9', ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when 'x3 y3'
-          @player_moves << 9
-          move = @all_lines_field[10].sub('9', 'X')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', ' ').sub('8', ' ').sub('9', 'X')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-      end
-
-      # Варианты при которых юзер выигрывает
-      positions_wins.each_value do |win|
-
-        if win - @player_moves == []
-          puts "Вы выиграли"
-          abort
-        end
-      end
-
-      if @comp_moves.size != 5
-        puts 'Ход компьютера.'
-        sleep 1.5
-        cls
-        comp_move = rand(1..9)
-
-        while @comp_moves.include?(comp_move) ||
-            @player_moves.include?(comp_move) do
-          comp_move = rand(1..9)
-        end
-        @comp_moves << comp_move
-        puts "Ходы компьютера: #{@comp_moves}"
-      else
-        puts 'Ничья.'
+      elsif comp_or_player == @comp_moves &&
+          win - comp_or_player == []
+        puts "Выиграл компьютер."
         abort
       end
-
-      case comp_move.to_s
-        when "1"
-          move = @all_lines_field[2].sub('1', 'O')
-          @all_lines_field[2] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub('2', ' ').sub('3', ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "2"
-          move = @all_lines_field[2].sub('2', 'O')
-          @all_lines_field[2] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').sub('2', 'O').sub('3', ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "3"
-          move = @all_lines_field[2].sub('3', 'O')
-          @all_lines_field[2] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').sub('2', ' ').sub('3', 'O')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "4"
-          move = @all_lines_field[6].sub('4', 'O')
-          @all_lines_field[6] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', 'O').sub('5', ' ').sub('6', ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "5"
-          move = @all_lines_field[6].sub('5', 'O')
-          @all_lines_field[6] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', ' ').sub('5', 'O').sub('6', ' ')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "6"
-          move = @all_lines_field[6].sub('6', 'O')
-          @all_lines_field[6] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].sub('4', ' ').sub('5', ' ').sub('6', 'O')
-          field_line10 = all_lines_field_new[10].gsub(/[789]/, ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "7"
-          move = @all_lines_field[10].sub('7', 'O')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', 'O').sub('8', ' ').sub('9', ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "8"
-          move = @all_lines_field[10].sub('8', 'O')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', ' ').sub('8', 'O').sub('9', ' ')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-        when "9"
-          move = @all_lines_field[10].sub('9', 'O')
-          @all_lines_field[10] = move
-
-          all_lines_field_new = Array.new(@all_lines_field)
-          field_line2 = all_lines_field_new[2].sub(/(.*)1(.*)1(.*)/, '\1 \21\3').gsub(/[23]/, ' ')
-          field_line6 = all_lines_field_new[6].gsub(/[456]/, ' ')
-          field_line10 = all_lines_field_new[10].sub('7', ' ').sub('8', ' ').sub('9', 'O')
-          all_lines_field_new[2] = field_line2
-          all_lines_field_new[6] = field_line6
-          all_lines_field_new[10] = field_line10
-
-          puts all_lines_field_new
-      end
-
-      # Варианты при которых comp выигрывает
-      positions_wins.each_value do |win|
-
-        if win - @comp_moves == []
-          puts 'Выиграл компьютер.'
-          abort
-        end
-      end
-
-    end # финал While
-
+    end
+  end
+  # гетеры
+  def all_moves
+    @all_moves
   end
 
-  # Финал метода player_vs_comp
-
-  def comp_vs_player # Компьютер против юзера
-    puts "\nКомпьютер ходит.\n"
-    sleep 2
+  def player_moves
+    @player_moves
   end
 
-  # Финал метода comp_vs_player
-
-  # Игрок против другого игрока
-  def player_vs_player
-    UnicodeUtils.downcase("D")
-    puts "\nКак делать ход! Введите (лат.) x1 y1 и Enter\n\nСделайте ваш ход.\n\n"
-  end # Финал метода player_vs_player
-
-  # Метод очистки экрана
-  def cls
-    system "clear" or system "cls"
+  def comp_moves
+    @comp_moves
   end
 end # Финал класса
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -7,47 +7,70 @@ if (Gem.win_platform?) # если винда
   end
 end
 # Присоединяю файлы
-require './screensaver.rb'
-require './opponent.rb'
-require './game.rb'
+require_relative 'screensaver.rb'
+require_relative 'game.rb'
+# gem unicode_utils для преобразования в нижний регистр слова и букв вводимых юзером
+require "unicode_utils/downcase"
+
 # Объявляю переменные с классами
 show = Screensaver.new
-gamer_choise = Opponent.new
 game = Game.new
-show.cls # Чищу экран
 show.show_screen # Вывожу заставку на экран
 show.cls # Удаляю заставку
 # Вывожу игровое поле на экран
 game.field
-# Спрашиваю у пользовател с кем он хочет играть 1)Компьютер 2)Другой игрок
-	# Если выбран вариант с компьютером
-	if gamer_choise.choice_of_opponent == 1   		
-		# Спрашиваю чем он хочет играть Х или О
-		# В зависимости от варианта выбора начинаю игру
-		# Если юзер выбрал крестики
-		if gamer_choise.what_we_play1 == 1
-		# Запускаю игру player_vs_comp
-		show.cls # Чищу экран
-		game.field # Вывожу игровое поле на экран
-		game.player_vs_comp
-		# Если юзер выбрал нолики
-    else
-		show.cls
-		game.field
-		game.comp_vs_player
-		end
-	# Если выбран вариант с другим игроком
-	else
-		if gamer_choise.what_we_play2 == 1
-			show.cls
-			puts "\n\r#{gamer_choise.name1} Ваш ход первый.\n"
-			game.field
-			game.player_vs_player
-		else
-			show.cls
-			puts "\n\r#{gamer_choise.name2} Ваш ход первый.\n"
-			game.field
-			game.player_vs_player
-		end	
+# пока пользователь не сделал 5 ходов
+while game.player_moves.size != 5
+	# Прошу пользователя сделать ход
+	puts "\n\rСделайте ваш ход. (Как делать ход: x1 y1 и Enter)\n"
+	move_player = STDIN.gets.chomp.downcase
+	# Проверка, чтобы юзер не ввел ерунду
+	while !game.all_moves.value?(move_player) do
+		puts "\nСделайте ваш ход. (Как делать ход: x1 y1 и Enter)\n"
+		move_player = STDIN.gets.chomp.downcase
 	end
+	# из ходов юзера делаю цифры
+	case move_player
+		when 'x1 y1'
+			move_player = 1
+		when 'x2 y1'
+			move_player = 2
+		when 'x3 y1'
+			move_player = 3
+		when 'x1 y2'
+			move_player =4
+		when 'x2 y2'
+			move_player = 5
+		when 'x3 y2'
+			move_player = 6
+		when 'x1 y3'
+			move_player = 7
+		when 'x2 y3'
+			move_player = 8
+		when 'x3 y3'
+			move_player = 9
+	end
+  # Вывожу ход юзера на игровое поле
+	game.move_player(move_player)
+	# проверяю не выиграл ли юзер на этом ходу
+	game.win(game.player_moves)
+
+	puts "Ход компьютера."
+	comp_move = rand(1..9)
+  # Пока ходы компа будут совподать с уже сделанными ходами
+	while game.comp_moves.include?(comp_move) || game.player_moves.include?(comp_move) do
+    # комп будет искать вариант хода
+		comp_move = rand(1..9)
+  end
+  # Вывожу ход компа на игровое поле
+	game.move_comp(comp_move)
+	# проверяю не выиграл ли комп на этом ходу
+	game.win(game.comp_moves)
+	# Если юзер походил 4 раза и не выиграл, то продолжать
+  # дальше смысла нет
+	if game.player_moves.size == 4
+		puts 'Ничья'
+		abort
+  end
+end
 	
